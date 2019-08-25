b5d4891811b40cf987c44be35361d4a2ddf0b1fd
Soumya Benni <soumyasoms@yahoo.com>
2019-08-26 01:44:24 +0530

    Android login page edited with my request page

--- Android/Login/login/src/main/java/com/example/administrator/login/login/BugerMenu.java
+++ Android/Login/login/src/main/java/com/example/administrator/login/login/BugerMenu.java
@@ -12,12 +12,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class BugerMenu extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+   // ListView listview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_forgot_password);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buger_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
