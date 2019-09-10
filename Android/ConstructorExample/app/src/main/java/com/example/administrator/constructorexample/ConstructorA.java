166240dd3b12a438e0e5e032d618ece901e9dbbe
Soumya Benni <soumyasoms@yahoo.com>
2019-09-10 15:20:39 +0530

    Constructor Task
    Uses Permission, object creation, method call from different class through object

--- Android/ConstructorExample/app/src/main/java/com/example/administrator/constructorexample/ConstructorA.java
+++ Android/ConstructorExample/app/src/main/java/com/example/administrator/constructorexample/ConstructorA.java
@@ -2,19 +2,21 @@ package com.example.administrator.constructorexample;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-public class ConstructorA extends AppCompactActivity {
+public class ConstructorA extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    ConstructorA() {
+    }
 
-        ConstructorA(){
-   // Toast.makeText(getApplication.Context(),"Constructor created");
-}
-public void object1(Context applicationContext){
-            Toast.makeText(applicationContext ,"This is Object", Toast,Length_Long).show();
-        }
+
+    public void Object1(Context applicationContext) {
+        Toast.makeText(applicationContext, "This is Object",Toast.LENGTH_LONG).show();
+    }
 }
--- Android/ConstructorExample/app/src/main/java/com/example/administrator/constructorexample/MainActivity.java
+++ Android/ConstructorExample/app/src/main/java/com/example/administrator/constructorexample/MainActivity.java
@@ -3,22 +3,28 @@ package com.example.administrator.constructorexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button=findViewbyId(R.id.button);
-        button.SetOnClickListener(new View.OnClickListener()){
-            public void Onclick(View View){
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 ConstructorA A=new ConstructorA();
                 A.Object1(getApplicationContext());
+
             }
-        }
+        });
+
+
+
 
     }
 
