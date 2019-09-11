3ddc8b89dc2843dd634324a9cb82136d430dd462
Soumya Benni <soumyasoms@yahoo.com>
2019-09-11 12:04:28 +0530

    edited with add and delete function

--- Android/ArrayExample/app/src/main/java/com/example/administrator/arrayexample/MainActivity.java
+++ Android/ArrayExample/app/src/main/java/com/example/administrator/arrayexample/MainActivity.java
@@ -9,43 +9,53 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Button add;
-    Button delete;
+    Button delete1;
     Button display;
     EditText text1;
     EditText text2;
-    EditText text3;
+    TextView text3;
 
-    private static final String TAG ="MainActivity";
     ArrayList<String> arrayList=new ArrayList<>();
-  //  List<String> Values = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        add.findViewById(R.id.add);
-        delete.findViewById(R.id.delete);
-        display.findViewById(R.id.display);
-        text1.findViewById(R.id.text1);
-        text3.findViewById(R.id.text3);
+        add=findViewById(R.id.add);
+        delete1=findViewById(R.id.delete);
+        display=findViewById(R.id.display);
+        text1=findViewById(R.id.text1);
+        text3=findViewById(R.id.text3);
 
-display.setOnClickListener(new View.OnClickListener() {
+add.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        arrayList.add("hello");
+       arrayList.add("world");
+    }
+});
+
+delete1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        arrayList.remove(0);
+    }
+});
+       display.setOnClickListener(new View.OnClickListener() {
  @Override
    public void onClick(View v) {
-        arrayList.add("hello");
-        arrayList.add("world");
-
-
         for (int i=0; i< arrayList.size(); i++){
             // Log.d(TAG," gjgvg"+arrayList.get(i));
-            text3.setText(text3.getText() + arrayList.get(i) + " , ");
+            text3.setText(text3.getText() + arrayList.get(i) + "  ");
 
         }
     }
