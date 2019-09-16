2fa35be85e6543885aced284fafbb79895f93105
Soumya Benni <soumyasoms@yahoo.com>
2019-09-16 18:09:10 +0530

    ListView Example

---
+++ Android/ListViewExample/app/src/main/java/com/example/listviewexample/AdaptorFile.java
@@ -0,0 +1,11 @@
+package com.example.listviewexample;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+class AdaptorFile {
+    public AdaptorFile(Context context, ArrayList<String> arrayList, ArrayList<String> arrayList1, ArrayList<String> arrayList2) {
+
+    }
+}
---
+++ Android/ListViewExample/app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.listviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> arrayList,arrayList1,arrayList2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView =findViewById(R.id.listView1);
+
+        arrayList=new ArrayList<>();
+        arrayList1=new ArrayList<>();
+        arrayList2=new ArrayList<>();
+
+arrayList.add("PUR - 2019 - 056");
+arrayList.add("PUR - 2019 - 057");
+arrayList.add("PUR - 2019 - 058");
+
+arrayList1.add("06 Jul 2019");
+arrayList1.add("06 Aug 2019");
+arrayList1.add("06 sep 2019");
+
+arrayList2.add("APPROVED");
+arrayList2.add("REJECTED");
+arrayList2.add("DRAFT");
+
+        AdaptorFile adaptorfile=new AdaptorFile(getApplicationContext(),arrayList,arrayList1,arrayList2);
+        listView.setAdapter(adaptorfile);
+
+
+
+
+    }
+}
