55b398421fd9d44c7c33caadac284af60f9e7cfb
Soumya Benni <soumyasoms@yahoo.com>
2019-09-18 12:53:06 +0530

    Requisition and ListView example

--- Android/ListViewExample/app/src/main/java/com/example/listviewexample/AdaptorFile.java
+++ Android/ListViewExample/app/src/main/java/com/example/listviewexample/AdaptorFile.java
@@ -1,11 +1,60 @@
 package com.example.listviewexample;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
-class AdaptorFile {
-    public AdaptorFile(Context context, ArrayList<String> arrayList, ArrayList<String> arrayList1, ArrayList<String> arrayList2) {
+class AdaptorFile extends BaseAdapter {
+    ArrayList<String> arrayslist,arraysList1,arraysList2;
+private Context context;
 
+    public AdaptorFile(Context context, ArrayList<String> arrayList, ArrayList<String> arrayList1, ArrayList<String> arrayList2) {
+this.context=context;
+this.arrayslist=arrayList;
+this.arraysList1=arrayList1;
+this.arraysList2=arrayList2;
+    }
+
+    @Override
+    public int getCount() {
+        return arrayslist.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View view1= LayoutInflater.from(context).inflate(R.layout.listitems,viewGroup,false);
+
+        TextView textView,textView1,textView2;
+        ImageView imageView;
+
+textView=view1.findViewById(R.id.textView);
+textView1=view1.findViewById(R.id.textView3);
+textView2=view1.findViewById(R.id.textView4);
+imageView=view1.findViewById(R.id.notification);
+
+textView.setText(arrayslist.get(i));
+textView1.setText(arraysList1.get(i));
+textView2.setText(arraysList2.get(i));
+imageView.setImageResource(R.drawable.notification);
+
+
+        return view1;
     }
 }
--- Android/ListViewExample/app/src/main/java/com/example/listviewexample/MainActivity.java
+++ Android/ListViewExample/app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -11,12 +13,14 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+
     ArrayList<String> arrayList,arrayList1,arrayList2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView =findViewById(R.id.listView1);
+
 
         arrayList=new ArrayList<>();
         arrayList1=new ArrayList<>();
@@ -34,7 +38,7 @@ arrayList2.add("APPROVED");
 arrayList2.add("REJECTED");
 arrayList2.add("DRAFT");
 
-        AdaptorFile adaptorfile=new AdaptorFile(getApplicationContext(),arrayList,arrayList1,arrayList2);
+        ListAdapter adaptorfile=new AdaptorFile(getApplicationContext(),arrayList,arrayList1,arrayList2);
         listView.setAdapter(adaptorfile);
 
 
